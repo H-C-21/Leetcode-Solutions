f6d87b6457a6bc1d6719d3f3126ec54a6b8ab1b7
Harshit <97081728+H-C-21@users.noreply.github.com>
2024-01-16 03:06:24 +0530

    Time: 410 ms (19.29%), Space: 27 MB (12.63%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int beautySum(string s) {
+       
+    if(s.size() < 3){
+        return 0;
+    }
+        
+        long long count=0;
+        
+        for(int i=0; i<s.size(); i++){
+            map<char,int> freq;
+      
+            for(int j=i; j<s.size(); j++){
+                freq[s[j]]++;
+               
+                int most = INT_MIN;
+                int least = INT_MAX;
+                    
+                 for (auto it = freq.begin(); it != freq.end(); it++) {
+                        least = min(least,it->second); // Update the minimum frequency
+                        most = max(most,it->second); // Update the maximum frequency
+                    }
+                
+                
+                    
+                    count += (most-least);
+                
+                
+                
+            }
+        }
+        
+        return count;
+        
+        
+        
+    }
+};
