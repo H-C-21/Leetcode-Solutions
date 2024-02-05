2c1b86c2b7973d29ef73f7b90f7f90746b18c078
Harshit <97081728+H-C-21@users.noreply.github.com>
2024-02-05 15:41:21 +0530

    Time: 19 ms (90.75%), Space: 11.7 MB (31.20%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char,int> rec;
+        
+        int rec[26] = {0};
         
         for(int i = 0;i<s.size();i++){
-            if(rec.find(s[i]) == rec.end()){
-                
-                rec[s[i]] = i+1;
-                
+            if(rec[s[i]-'a'] == 0){
+                rec[s[i]-'a'] = i+1;
             } else {
-                rec[s[i]] = -1;
+                rec[s[i]-'a'] = -1;
             }
             // cout<<s[i]<<" "<<rec[s[i]]<<endl;
         }
-
-        for(int i=0;i<s.size(); i++){
-            if(rec[s[i]] != -1){
-                return i;
+        
+        int ans = INT_MAX;
+        for(int i = 0; i<26; i++){
+            if(rec[i] > 0){
+                ans = min(ans,rec[i]-1);
             }
+        }
+        if(ans != INT_MAX){
+            return ans;
         }
 
     return -1;
